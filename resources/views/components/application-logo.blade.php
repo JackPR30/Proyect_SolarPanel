5d536b1f11e758c1ad2427b7daec4e4daaaa8616
Jack Portilla <jackportilla24@gmail.com>
2023-12-02 12:39:48 -0500

    Proyecto IOT

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <img  style="width: 200px; height: auto;" src="{{ asset('img/logo.png') }}" alt="Logo proyecto">
+</svg>
